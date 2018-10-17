55c795727e81e7920ae9211ef91c69cd37c45db9
sirius19 <wang220012@126.com>
2018-10-17 23:05:49 +0800

    test

--- src/test/java/com/wqb/test/ElasticsearchTemplateTest.java
+++ src/test/java/com/wqb/test/ElasticsearchTemplateTest.java
@@ -6,10 +6,13 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHits;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
@@ -23,15 +26,41 @@ public class ElasticsearchTemplateTest {
     @Resource
     private ElasticsearchTemplate elasticsearchTemplate;
 
-    @Test
-    public void test(){
-        Client client = elasticsearchTemplate.getClient();
-        SearchRequestBuilder searchRequestBuilder = client.prepareSearch("call_think_log")
+    private Client client;
+    private SearchRequestBuilder searchRequestBuilder;
+    @Before
+    public void testBefore(){
+        client = elasticsearchTemplate.getClient();
+        searchRequestBuilder = client.prepareSearch("call_think_log_201810")
                 .setTypes("call_think_log");
+    }
+
+    @Test
+    public void matchAllTest() {
         SearchResponse searchResponse = searchRequestBuilder.setQuery(QueryBuilders.matchAllQuery()).execute().actionGet();
         SearchHits searchHits = searchResponse.getHits();
-        searchHits.forEach(hit->{
+        searchHits.forEach(hit -> {
             System.out.println(hit.getSourceAsString());
         });
+    }
+
+    @Test
+    public void emptySearchTest() {
+        SearchResponse searchResponse = searchRequestBuilder.setQuery(QueryBuilders.matchAllQuery()).setFrom(10)
+                .setSize(5).execute().actionGet();
+        SearchHits searchHits = searchResponse.getHits();
+        searchHits.forEach(hit -> {
+            System.out.println(hit.getSourceAsString());
+        });
+    }
+
+    @Test
+    public void constantScoreTest() {
+        SearchQuery searchQuery = new NativeSearchQueryBuilder().
+                withQuery(QueryBuilders.constantScoreQuery(QueryBuilders
+                        .termQuery("cus_phone", "13882314639")).boost(2.0f))
+                .build();
+        searchRequestBuilder.setQuery(searchQuery.getQuery());
+        System.out.println(searchRequestBuilder.toString());
     }
 }
